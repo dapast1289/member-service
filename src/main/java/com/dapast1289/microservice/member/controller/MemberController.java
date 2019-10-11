226c5d22023e349ffc295ea8da6e6229b20ee58d
Leo <dapast1289@gmail.com>
2019-10-11 18:07:52 +0800

    update swagger description

--- src/main/java/com/dapast1289/microservice/member/controller/MemberController.java
+++ src/main/java/com/dapast1289/microservice/member/controller/MemberController.java
@@ -2,7 +2,6 @@ package com.dapast1289.microservice.member.controller;
 
 import com.dapast1289.microservice.member.entity.MemberEntity;
 import com.dapast1289.microservice.member.service.MemberService;
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,16 +15,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "")
-@Api(value = "mem")
 public class MemberController {
 
     @Autowired
     private MemberService memberService;
 
-    @ApiOperation(value = "取得書本", notes = "列出所有書本")
+    @ApiOperation(value = "get all member list", notes = "")
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<MemberEntity>> index() {
-
         List<MemberEntity> allMembers = memberService.getAll();
         return new ResponseEntity<>(allMembers, HttpStatus.OK);
     }
